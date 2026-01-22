870fae15a10a580c7b0a41d1a9b9e016f8361ef8
rladjwls02 <safhao7151@naver.com>
2026-01-23 02:03:38 +0900

    Initial commit

---
+++ moomook/src/main/java/com/safhao/moomook/MoomookApplication.java
@@ -0,0 +1,13 @@
+package com.safhao.moomook;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MoomookApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MoomookApplication.class, args);
+	}
+
+}
---
+++ moomook/src/test/java/com/safhao/moomook/MoomookApplicationTests.java
@@ -0,0 +1,13 @@
+package com.safhao.moomook;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MoomookApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
