4df1987e0e3b3d8a98562281d692bdaa990947dd
rladjwls02 <safhao7151@naver.com>
2026-01-23 03:40:05 +0900

    프로젝트 초기화

--- moomook/src/main/java/com/safhao/moomook/menu/MenuRepository.java
+++
@@ -1,8 +0,0 @@
-package com.safhao.moomook.menu;
-
-import java.util.List;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface MenuRepository extends JpaRepository<Menu, Long> {
-    List<Menu> findAllByAvailableTrue();
-}
